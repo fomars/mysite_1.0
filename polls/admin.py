ff7c570972ef8f557d6ee13d1575d600d720ae4d
Arseniy <fomars@gmail.com>
2014-08-23 19:49:04 +0400

    urls

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from polls.models import Poll, Choice
 
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']})
+    ]
+    inlines = [ChoiceInLine]
+# class ChoiceAdmin(admin.ModelAdmin):
+
+
+admin.site.register(Poll, PollAdmin)
 # Register your models here.
